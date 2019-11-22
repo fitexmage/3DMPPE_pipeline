06448a3e5bd06e2aefe28aff2d14e990e1f09a69
Shengguang Bai <fitexmage@shengguangs-mbp.home>
2019-11-22 02:57:13 -0500

    debug

--- main.py
+++ main.py
@@ -8,6 +8,15 @@ from detectron2.config import get_cfg
 from detectron2.utils.visualizer import Visualizer
 from detectron2.data import MetadataCatalog
 
+import argparse
+from rootnet_repo.main.config import cfg
+import torch
+from rootnet_repo.common.base import Tester
+from rootnet_repo.common.utils.pose_utils import flip
+import torch.backends.cudnn as cudnn
+import math
+import torchvision.transforms as transforms
+
 def main():
     im = cv2.imread("./input.jpg")
 
@@ -20,20 +29,19 @@ def main():
     outputs = predictor(im)
 
     person_boxes = outputs["instances"].pred_boxes[outputs["instances"].pred_classes == 0]
-    person_images = []
-    for box in person_boxes:
-        box = box.cpu().numpy().astype(int)
+    person_images = np.array((len(person_boxes), 3, 256, 256))
+    k_values = np.array(len(person_boxes), 1)
+    for i in range(len(person_boxes)):
+        box = person_boxes[i].cpu().numpy().astype(int)
         image = im[box[1]:box[3], box[0]: box[2]]
-        person_images.append(image)
+        image = cv2.resize(image, (256, 256))
+        image = np.transpose(image, (2, 0, 1))
+        person_images[i] = image
 
-    import argparse
-    from rootnet_repo.main.config import cfg
-    import torch
-    from rootnet_repo.common.base import Tester
-    from rootnet_repo.common.utils.pose_utils import flip
-    import torch.backends.cudnn as cudnn
-    import math
-    import torchvision.transforms as transforms
+        k_values[i] = np.array([math.sqrt(2000 * 2000 * 30 * 30 / (image.shape[1] * image.shape[2]))]).astype(np.float32)
+
+    person_images = torch.Tensor(person_images)
+    k_values = torch.Tensor(k_values)
 
     cfg.set_args(gpu_ids='0')
     cudnn.fastest = True
@@ -46,26 +54,18 @@ def main():
 
     preds = []
 
-    for image in person_images:
-        k_value = np.array([math.sqrt(2000 * 2000 * 30 * 30 / (image.shape[0] * image.shape[1]))]).astype(np.float32)
-        k_value = torch.Tensor(k_value)
+    transform = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize(mean=cfg.pixel_mean, std=cfg.pixel_std)]
+    )
+    person_images = transform(person_images)
 
-        image = cv2.resize(image, (256, 256))
-        cv2.imwrite("output.jpg", image)
-        break
-        transform = transforms.Compose([ \
-            transforms.ToTensor(),
-            transforms.Normalize(mean=cfg.pixel_mean, std=cfg.pixel_std)] \
-            )
-        image = transform(image)
-        image = torch.unsqueeze(image, 0)
-
-        with torch.no_grad():
-            coord_out = tester.model(image, k_value)
-            coord_out = coord_out.cpu().numpy()
-            preds.append(coord_out)
-    # preds = np.concatenate(preds, axis=0)
-    # print(preds)
+    with torch.no_grad():
+        coord_out = tester.model(person_images, k_values)
+        coord_out = coord_out.cpu().numpy()
+        preds.append(coord_out)
+    preds = np.concatenate(preds, axis=0)
+    print(preds)
 
 if __name__ == "__main__":
     main()
