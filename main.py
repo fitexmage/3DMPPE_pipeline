0d94197124c248631173e4f5feb402c98b55cd57
Shengguang Bai <fitexmage@shengguangs-mbp.home>
2019-11-21 22:48:01 -0500

    debug

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ def main():
     person_boxes = outputs["instances"].pred_boxes[outputs["instances"].pred_classes == 0]
     person_images = []
     for box in person_boxes:
-        box = box.tensor
+        box = box.numpy()
         image = im[box[0]:box[2], box[1]: box[3]]
         person_images.append(image)
     cv2.imwrite("output.jpg", person_images[0])
