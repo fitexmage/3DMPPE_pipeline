b2b24ba493fd31292e1cf482bec6b93a06aa88d6
Shengguang Bai <fitexmage@shengguangs-mbp.home>
2019-11-22 00:18:33 -0500

    Add rootnet.

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
-import detectron2
-
 # import some common libraries
 import numpy as np
 import cv2
 import random
 
-# import some common detectron2 utilities
 from detectron2.engine import DefaultPredictor
 from detectron2.config import get_cfg
 from detectron2.utils.visualizer import Visualizer
 from detectron2.data import MetadataCatalog
+
+import argparse
+from tqdm import tqdm
+from rootnet_repo.main.config import cfg
+import torch
+from rootnet_repo.common.base import Tester
+from rootnet_repo.common.utils.vis import vis_keypoints
+from rootnet_repo.common.utils.pose_utils import flip
+import torch.backends.cudnn as cudnn
 
 def main():
     im = cv2.imread("./input.jpg")
@@ -26,7 +32,27 @@ def main():
         box = box.cpu().numpy().astype(int)
         image = im[box[1]:box[3], box[0]: box[2]]
         person_images.append(image)
-    cv2.imwrite("output.jpg", person_images[0])
+
+    test_img = person_images[0]
+
+    parser = argparse.ArgumentParser()
+    args = parser.parse_args()
+    cfg.set_args(args.gpu_ids)
+    cudnn.fastest = True
+    cudnn.benchmark = True
+    cudnn.deterministic = False
+    cudnn.enabled = True
+
+    tester = Tester(args.test_epoch)
+    tester._make_batch_generator()
+    tester._make_model()
+
+    preds = []
+
+    with torch.no_grad():
+        coord_out = tester.model(test_img)
+        print(type(coord_out))
+
 
 if __name__ == "__main__":
     main()
