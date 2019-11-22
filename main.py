1768e93eae25c41e8448d0746c6968c5c044b7b2
Shengguang Bai <fitexmage@shengguangs-mbp.home>
2019-11-22 03:47:24 -0500

    debug

--- main.py
+++ main.py
@@ -44,7 +44,7 @@ def main():
     for box in person_boxes:
         box = box.cpu().numpy().astype(int)
         # image = im[box[1]:box[3], box[0]: box[2]]
-        image = generate_patch_image(im, box, False, 0)
+        image,  = generate_patch_image(im, box, False, 0)
         k_values[i] = np.array([math.sqrt(2000 * 2000 * 30 * 30 / (image.shape[1] * image.shape[2]))]).astype(np.float32)
         image = transform(image)
         person_images[i, :, :image.shape[1], :image.shape[2]] = image
