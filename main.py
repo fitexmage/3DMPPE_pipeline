a4748a8f8e5a99f22e3efa2b9ffaef3147c46076
Shengguang Bai <fitexmage@shengguangs-mbp.home>
2019-11-22 02:58:03 -0500

    debug

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ def main():
 
     person_boxes = outputs["instances"].pred_boxes[outputs["instances"].pred_classes == 0]
     person_images = np.array((len(person_boxes), 3, 256, 256))
-    k_values = np.array(len(person_boxes), 1)
+    k_values = np.array((len(person_boxes), 1))
     for i in range(len(person_boxes)):
         box = person_boxes[i].cpu().numpy().astype(int)
         image = im[box[1]:box[3], box[0]: box[2]]
