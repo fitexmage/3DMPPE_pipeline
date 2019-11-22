6611e7c72c31527187e26c17c896cad98d569e3b
Shengguang Bai <fitexmage@shengguangs-mbp.home>
2019-11-22 04:12:36 -0500

    debug

--- main.py
+++ main.py
@@ -43,11 +43,11 @@ def main():
 
     for box in person_boxes:
         box = box.cpu().numpy().astype(int)
-        k_values[i] = np.array([math.sqrt(2000 * 2000 * 35 * 35 / ((box[3] - box[1]) * (box[2] - box[0])))]).astype(
-            np.float32)
         image, _ = generate_patch_image(im, box, False, 0)
+        cv2.imwrite("output.jpg", image)
         image = transform(image)
         person_images[i] = image
+        k_values[i] = np.array([math.sqrt(2000 * 2000 * 35 * 35 / ((box[3] - box[1]) * (box[2] - box[0])))]).astype(np.float32)
 
         i += 1
 
