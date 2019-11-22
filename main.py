4cfaa5bd646db7c64245ac8ddb43768c06de6a2e
Shengguang Bai <fitexmage@shengguangs-mbp.home>
2019-11-22 15:54:50 -0500

    debug

--- main.py
+++ main.py
@@ -87,7 +87,9 @@ def main():
         posenet_preds = posenet_preds.cpu().numpy()
         print(posenet_preds[0])
         for i, box in enumerate(person_boxes):
-            cv2.circle(im, (box[0] + posenet_preds[i][0], box[1] + posenet_preds[i][1]), 5, (0, 0, 255), 0)
+            posenet_preds[i][0] += box[0]
+            posenet_preds[i][1] += box[1]
+            cv2.circle(im, (posenet_preds[i][0], posenet_preds[i][1]), 5, (0, 0, 255), 0)
     cv2.imwrite("output.jpg", im)
 
 if __name__ == "__main__":
