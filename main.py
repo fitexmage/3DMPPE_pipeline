537ed05089209774a6e3e3f861a6504559336f1c
Shengguang Bai <fitexmage@shengguangs-mbp.home>
2019-11-22 03:00:11 -0500

    debug

--- main.py
+++ main.py
@@ -31,14 +31,16 @@ def main():
     person_boxes = outputs["instances"].pred_boxes[outputs["instances"].pred_classes == 0]
     person_images = np.array((len(person_boxes), 3, 256, 256))
     k_values = np.array((len(person_boxes), 1))
-    for i in range(len(person_boxes)):
-        box = person_boxes[i].cpu().numpy().astype(int)
+    i = 0
+    for box in person_boxes:
+        box = box.cpu().numpy().astype(int)
         image = im[box[1]:box[3], box[0]: box[2]]
         image = cv2.resize(image, (256, 256))
         image = np.transpose(image, (2, 0, 1))
         person_images[i] = image
-
         k_values[i] = np.array([math.sqrt(2000 * 2000 * 30 * 30 / (image.shape[1] * image.shape[2]))]).astype(np.float32)
+
+        i += 1
 
     person_images = torch.Tensor(person_images)
     k_values = torch.Tensor(k_values)
