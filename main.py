2de53960e1b0c4c02d875ccb1643b976d4c915f3
Shengguang Bai <fitexmage@shengguangs-mbp.home>
2019-11-22 03:24:00 -0500

    debug

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ def main():
 
     person_boxes = outputs["instances"].pred_boxes[outputs["instances"].pred_classes == 0]
 
-    person_images = np.zeros((len(person_boxes), 256, 256, 3))
+    person_images = np.zeros((len(person_boxes), 3, 256, 256))
     k_values = np.zeros((len(person_boxes), 1))
 
     transform = transforms.Compose([
@@ -46,7 +46,6 @@ def main():
         image = cv2.resize(image, (256, 256))
         # image = np.transpose(image, (2, 0, 1))
         image = transform(image)
-        print(image.shape)
         person_images[i] = image
         k_values[i] = np.array([math.sqrt(2000 * 2000 * 30 * 30 / (image.shape[1] * image.shape[2]))]).astype(np.float32)
 
