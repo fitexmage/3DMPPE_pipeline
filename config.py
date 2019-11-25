bea12eadedb5ef627bffb67a04466c7c9fe716d5
Shengguang Bai <fitexmage@shengguangs-mbp.home>
2019-11-25 17:21:31 -0500

    debug

--- config.py
+++ config.py
@@ -53,7 +53,7 @@ class Config:
     flip_pairs = ((2, 5), (3, 6), (4, 7), (8, 11), (9, 12), (10, 13))
     rootnet_model_inx = 18
     posenet_model_inx = 24
-    f = np.array([1300, 1300])
+    f = np.array([800, 800])
 
     to_camera = True
     vis = True
