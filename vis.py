d3b491bba1e3dc71abd0d5f7b0e36fa611f75916
Shengguang Bai <fitexmage@shengguangs-mbp.home>
2019-11-25 14:33:27 -0500

    Fix an import bug.

--- vis.py
+++ vis.py
@@ -12,7 +12,7 @@ def visualize_image(image, preds):
     tmpimg = tmpimg[::-1, :, :]
     tmpimg = np.transpose(tmpimg, (1, 2, 0)).copy()
     tmpkps = np.zeros((3, pipeline_cfg.joint_num))
-    tmpkps[:2, :] = preds[0, :, :2].cpu().numpy().transpose(1, 0) / pipeline_cfg.output_shape[0] * pipeline_cfg.input_shape[0]
+    tmpkps[:2, :] = preds[0, :, :2].cpu().numpy().transpose(1, 0) / posenet_cfg.output_shape[0] * posenet_cfg.input_shape[0]
     tmpkps[2, :] = 1
     tmpimg = vis_keypoints(tmpimg, tmpkps, pipeline_cfg.skeleton)
     cv2.imwrite('output.jpg', tmpimg)
