b8f5aab681b0f79fccb582a0a70b70509635ac9d
Shengguang Bai <fitexmage@shengguangs-mbp.home>
2019-11-25 14:58:24 -0500

    Change vis function.

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import cv2
 from detectnet import get_bounding_boxes
 from rootnet import get_input, get_root
 from posenet import get_pose
+from config import cfg as pipeline_cfg
+from vis import visualize
 
 def main():
     image = cv2.imread("./input.jpg")
@@ -15,6 +17,8 @@ def main():
     print(rootnet_preds.shape)
     posenet_preds = get_pose(person_boxes, person_images, rootnet_preds)
     print(posenet_preds.shape)
+    if pipeline_cfg.vis:
+        visualize(image, posenet_preds)
     return posenet_preds
 
 if __name__ == "__main__":
--- vis.py
+++ vis.py
@@ -5,14 +5,13 @@ from config import cfg as pipeline_cfg
 from posenet_repo.common.utils.vis import vis_keypoints, vis_3d_skeleton
 from posenet_repo.main.config import cfg as posenet_cfg
 
-def visualize_image(image, preds):
-    tmpimg = image[0].cpu().numpy()
-    tmpimg = tmpimg * np.array(posenet_cfg.pixel_std).reshape(3, 1, 1) + np.array(posenet_cfg.pixel_mean).reshape(3, 1, 1)
-    tmpimg = tmpimg.astype(np.uint8)
-    tmpimg = tmpimg[::-1, :, :]
-    tmpimg = np.transpose(tmpimg, (1, 2, 0)).copy()
-    tmpkps = np.zeros((3, pipeline_cfg.joint_num))
-    tmpkps[:2, :] = preds[0, :, :2].cpu().numpy().transpose(1, 0) / posenet_cfg.output_shape[0] * posenet_cfg.input_shape[0]
-    tmpkps[2, :] = 1
-    tmpimg = vis_keypoints(tmpimg, tmpkps, pipeline_cfg.skeleton)
+def visualize(image, preds):
+    if pipeline_cfg.to_camera:
+        print()
+    else:
+        tmpimg = image.clone()
+        for pred in preds:
+            tmpimg = vis_keypoints(tmpimg, np.transpose(pred), pipeline_cfg.skeleton)
+
     cv2.imwrite('output.jpg', tmpimg)
+
