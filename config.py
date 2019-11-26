cfabf6b9fbc2f32ac23de0c2138e4aa1c0b53077
Shengguang Bai <fitexmage@dhcp-rhodes-3228.redrover.cornell.edu>
2019-11-25 20:23:02 -0500

    debug

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import numpy as np
 
 
 class Config:
-    input_inx = 1
+    input_inx = 2
     input_path = "data/input_" + str(input_inx) + ".jpg"
     output_path = "data/output_" + str(input_inx) + ".jpg"
 
