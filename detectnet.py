699f5732b84317787e6e866f84847c25d50d20f8
Shengguang Bai <fitexmage@shengguangs-mbp.home>
2019-11-25 19:10:31 -0500

    Add new picture.

--- detectnet.py
+++ detectnet.py
@@ -25,7 +25,6 @@ def get_bounding_boxes(image):
         box = box.cpu().numpy()
         tmp_image = image[int(box[1]):int(box[3]), int(box[0]):int(box[2])]
         not_blur = cv2.Laplacian(cv2.cvtColor(tmp_image, cv2.COLOR_BGR2GRAY), cv2.CV_32F).var()
-        print(not_blur)
         if not_blur < 200:
             continue
         box = np.array([box[0], box[1], box[2] - box[0], box[3] - box[1]])
